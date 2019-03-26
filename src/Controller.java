3c638a7ea7a59cb0b4d2faa4802fcbeaea68318c
NCC74656 <wolginmark@gmail.com>
2019-03-25 20:14:13 -0400

    Da ada

--- src/Controller.java
+++ src/Controller.java
@@ -24,7 +24,7 @@ public class Controller implements ActionListener {
 	public void start(){
 		drawAction = new AbstractAction(){
     		public void actionPerformed(ActionEvent e){
-    			//increment the x and y coordinates, alter direction if necessary
+    			 //increment the x and y coordinates, alter direction if necessary
     			model.updateLocationAndDirection();
     			
     			//update the view
